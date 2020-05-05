9b7609581cd84a025e1e97799c9e25f645f5738d
Ihor <Tarabanovigor@gmail.com>
2020-05-05 22:57:58 +0300

    Revert "Revert "init""
    This reverts commit b2b2a2122e30fb5b8698374cfa73d238adb5ab94.

---
+++ BitTrader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitTrader
+//
+//  Created by Tarabanov Igor on 05.05.2020.
+//  Copyright © 2020 Tarabanov Igor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
